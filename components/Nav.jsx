b8669ba6bda7bddabe3404dbceeb3743a146019b
basemosman2 <49072081+basemosman2@users.noreply.github.com>
2023-07-06 23:35:25 +0100

    added google authentication to sigIn

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
 
-  const isUserLoggedIn = true ;
+  const {data : session} = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
@@ -19,6 +19,8 @@ const Nav = () => {
       setProviders(res);
     })();
   }, []);
+
+  console.log(session)
 
   return (
     <nav className=' flex-between w-full mb-16 pt-3'>
@@ -36,7 +38,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        { isUserLoggedIn ? (
+        { session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -48,7 +50,7 @@ const Nav = () => {
 
             <Link href='/profile'>
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -63,9 +65,7 @@ const Nav = () => {
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() => {
-                    signIn(provider.id);
-                  }}
+                  onClick={() => signIn(provider.id)}
                   className='black_btn'
                 >
                   Sign in
@@ -77,10 +77,10 @@ const Nav = () => {
 
      {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
+              src={session?.user?.image}
               width={37}
               height={37}
               className='rounded-full cursor-pointer'
